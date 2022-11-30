cb49e9402e833178a1c31b635f6b598bbb955950
JOHAN ALGER <91075976+AlgerJohan@users.noreply.github.com>
2022-11-30 18:19:42 +0100

    Mémo

---
+++ commande_de_base.js
@@ -0,0 +1,35 @@
+//Caractère "nouvelle ligne" interdit
+// console.log("Je suis la première ligne
+// Et je suis la deuxième ligne");
+
+//Guillmemets simple ou double ! Quand utiliser l'un ou l'autre ?
+// console.log('Javascript c'est super'); //Pas bon
+// console.log("Javascript c'est super"); //Bon
+// console.log("Javascript est "super" facile"); //Pas bon
+// console.log('Javascript est "super" facile'); //Bon
+
+
+//Echapper un caractère avec \
+console.log('Javascript c\'est "super" simple');
+console.log("Javascript c'est \"super\" simple");
+
+
+//Faire une nouvelle ligne '\n'
+console.log("Je suis la première ligne\nEt je suis la deuxième ligne");
+
+
+//Concaténation de chaînes (opérateur +)
+console.log("Bienvenue" +  "Vanessa");
+
+
+/* Nouveauté ES6 : les templates string (modèles de libellés)*/
+//Caractère accent grave ou "apostrophe inverse" (backticks)
+// Avantages : pas besoin d'échapper les guillements (seulement le backticks) + multi-lignes
+console.log(`Je suis une ' "chaîne" de caractères écrite avec backtiks \` ! `);
+
+
+//Inconvénient : disponible qu'à partir d'ES6
+
+
+
+/* Bonne pratiques */
